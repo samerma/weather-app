4ebb77371284871d6d774a80f62941bec95fb4b1
Samer <samahajna@gmail.com>
2020-05-16 01:32:16 +0300

    heroku setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const api = require('./server/routes/api')
 const path = require('path')
 
-mongoose.connect("mongodb://localhost/WeatherDB", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WeatherDB", { useNewUrlParser: true })
     .then(() => { console.log("Succesfully Connected to the Mongodb Database") })
     .catch(() => { console.log("Error Connecting to the Mongodb Database") })
 
@@ -15,17 +15,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use('/', api)
 
-/////-----load data to DB---/////////
-/* c = new City({
-    name: 'tlv',
-    temperature: 10,
-    condition: 'good',
-    conditionPic: 'pic'
-})
-c.save() */
-//////////////////////////////////
-
 const port = 3001
-app.listen(port, function () {
+app.listen(process.env.PORT || port, function () {
     console.log(`Running on port ${port}`)
 })
